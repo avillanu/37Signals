7f33b18850f30a6d00334d9c8914c1ca4655494e
Alex <alexv@Alexs-MacBook-Air.local>
2014-01-16 12:59:12 -0500

    a

--- application.js
+++ application.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('#panel1').mouseover(function() {
-    	document.getElementById('frustrationfree').innerHTML ='Are you still managing projects with email? Are you still using Excel for your todo-lists? Its time to upgrade to basecamp. Manage projects and collaborate with your team and clients the modern way.' this.style.fontSize: 14px this.style.fontFamily: "'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif";
+    	document.getElementById('frustrationfree').innerHTML ='Are you still managing projects with email? Are you still using Excel for your todo-lists? Its time to upgrade to basecamp. Manage projects and collaborate with your team and clients the modern way.' ;
+    	var frustrationfree = document.getElementById('frustrationfree')
+    	frustrationfree.style.fontSize: "14px";
     		
     });	
 });
