ea6a95bfda35beafc26ac6006621097f36173407
Alex <alexv@Alexs-MacBook-Air.local>
2014-01-16 13:05:26 -0500

    a

--- application.js
+++ application.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
     $('#panel1').mouseover(function() {
     	document.getElementById('frustrationfree').innerHTML ='Are you still managing projects with email? Are you still using Excel for your todo-lists? Its time to upgrade to basecamp. Manage projects and collaborate with your team and clients the modern way.' ;
-    	var ff = document.getElementById('frustrationfree');
-    	ff.style.fontSize: "14px";
+    	
     		
     });	
 });
