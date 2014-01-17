8b1d8ecce55f4e680377d3969e779c868b05b697
Alex <alexv@new-host.home>
2014-01-16 21:14:17 -0500

    A

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#panel1').hover(function() {
     	document.getElementById('frustrationfree').innerHTML ='Are you still managing projects with email? Are you still using Excel for your todo-lists? Its time to upgrade to basecamp. Manage projects and collaborate with your team and clients the modern way.' ;
     	document.getElementById('frustrationfree').style.fontSize = "14px";
-    	$('arrow1').show();
+    	$('#arrow1').show();
     },
     	function(){
     	document.getElementById('frustrationfree').innerHTML ='Frustration-free web-based apps for collaboration, sharing information, and making decision.';
